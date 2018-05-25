d928291aae8f627085ee761561b535ca133ca2d7
YeonwooSung <neos960518@gmail.com>
2018-05-25 12:34:52 +0900

    implemented echo command

---
+++ echo/main.c
@@ -0,0 +1,24 @@
+//
+// Created by YeonwooSung on 2018. 5. 25..
+//
+
+#include <stdio.h>
+
+/**
+ * The main function for the echo command.
+ *
+ * The aim of the echo command is to print out the given string.
+ *
+ * @param argc the number of command line arguments.
+ * @param argv the value of command line arguments.
+ * @return 0
+ */
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        for (unsigned int i = 1; i < argc) {
+            printf("%s", argv[i]);
+        }
+    }
+
+    return 0;
+}
