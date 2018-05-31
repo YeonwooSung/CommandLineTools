ae988557fe8b172834da73a9011283c2a9e8d42e
YeonwooSung <neos960518@gmail.com>
2018-06-01 00:31:19 +0900

    fixed minor issue in the echo command

--- echo/main.c
+++ echo/main.c
@@ -15,7 +15,7 @@
  */
 int main(int argc, char *argv[]) {
     if (argc > 1) { //check the number of command line arguments.
-        for (unsigned int i = 1; i < argc) {
+        for (unsigned int i = 1; i < argc; i++) {
             printf("%s\n", argv[i]);
         }
     }
