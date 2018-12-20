0e2d34dd7dd4d17fdfbd81996318caece29f32a7
YeonwooSung <neos960518@gmail.com>
2018-12-20 13:39:08 +0900

    implement the basic function of  chown

--- FilePermission/chown/main.c
+++ FilePermission/chown/main.c
@@ -4,8 +4,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <grp.h>
 #include <pwd.h>
 
@@ -18,16 +20,12 @@
  * This function uses the POSIX system call "chown" to change the user name and group name of the specific file.
  *
  * @param file_path the path of the target file.
- * @param user_name the new user name.
- * @param group_name the new group name.
+ * @param suppressMessage If 1, suppress all messages except error messages.
  */
-void doChown(const char *file_path, const char *user_name, const char *group_name) {
-    uid_t uid; //user id
-    gid_t gid; //group id
-    struct passwd *pwd;
-    struct group  *grp;
-
-    pwd = getpwnam(user_name);
+void doChown(const char *file_path, char suppressMessage) {
+    struct stat info;
+    stat(file_path, &info); //get the file stat
+    struct passwd *pwd = getpwuid(info.st_uid);
 
     //check if the user information is correct
     if (pwd == NULL) {
@@ -35,9 +33,7 @@ void doChown(const char *file_path, const char *user_name, const char *group_nam
         exit(0);
     }
 
-    uid = pwd->pw_uid;
-
-    grp = getgrnam(group_name);
+    struct group *grp = getgrgid(info.st_gid);
 
     //check if the group information is correct.
     if (grp == NULL) {
@@ -45,11 +41,20 @@ void doChown(const char *file_path, const char *user_name, const char *group_nam
         exit(0);
     }
 
+    uid_t uid; //user id
+    gid_t gid; //group id
+
+    uid = pwd->pw_uid;
+
     gid = grp->gr_gid;
 
     if (chown(file_path, uid, gid) == -1) {
         fprintf(stderr, FAILED);
         exit(0);
+    }
+
+    if (suppressMessage != 1) {
+        printf("chown success!\n");
     }
 }
 
@@ -59,8 +64,9 @@ void doChown(const char *file_path, const char *user_name, const char *group_nam
  * @param argc the argument count
  * @param argv the values of command line arguments
  * @param optFlag the option flag to check which options are selected.
+ * @param filePath the file path name.
  */
-void checkArguments(int argc, char *argv[], char *optFlag) {
+void checkArguments(int argc, char *argv[], char *optFlag, char *filePath) {
     int opt;
     char flag = 0;
 
@@ -69,12 +75,24 @@ void checkArguments(int argc, char *argv[], char *optFlag) {
         switch (opt) {
             case 'f' : //The -f option suppresses all error messages except usage message.
                 flag += 1;
+                if (optarg) {
+                    filePath = (char *) malloc(strlen(optarg) + 1);
+                    strcpy(filePath, optarg);
+                }
                 break;
             case 'h' : //Just change the owner name of the symbolic link, not the file that of the file pointed to by it.
                 flag += 2;
+                if (optarg) {
+                    filePath = (char *) malloc(strlen(optarg) + 1);
+                    strcpy(filePath, optarg);
+                }
                 break;
             case 'R' : //Descends directories recursively, changing the ownership for each file.
                 flag += 4;
+                if (optarg) {
+                    filePath = (char *) malloc(strlen(optarg) + 1);
+                    strcpy(filePath, optarg);
+                }
                 break;
             default:
                 fprintf(stderr, USAGE_MESSAGE);
@@ -96,23 +114,33 @@ void checkArguments(int argc, char *argv[], char *optFlag) {
 int main(int argc, char *argv[]) {
     if (argc <= 1) {
         fprintf(stderr, USAGE_MESSAGE);
+        exit(0);
     }
 
     char option = 0;
     char suppressMessages = 0;
+    char *filePath = NULL;
 
-    checkArguments(argc, argv, &option);
+    checkArguments(argc, argv, &option, filePath);
+
+    if (filePath == NULL) {
+        fprintf(stderr, USAGE_MESSAGE);
+        exit(0);
+    }
+
+    char hFlag = 0; //The "-h" option is for changing the owner of the symbolic link.
+    char rFlag = 0; //The "-R" option is for recursive chown.
 
     //check if the user used the -R option
     if (option >= 4) {
-        //TODO
+        rFlag = 1;
 
         option -= 4;
     }
 
     //check if the user used the -h option
     if (option >= 2) {
-        //TODO
+        hFlag = 1;
 
         option -= 2;
     }
@@ -122,6 +150,14 @@ int main(int argc, char *argv[]) {
         suppressMessages = 1;
 
         option -= 1;
+    }
+
+    if (rFlag) {
+        //TODO recursive chown
+    } else if (hFlag) {
+        //TODO change ownership of the encountered symbolic link
+    } else {
+        doChown(filePath, suppressMessages); //do chown
     }
 
     return 0;
